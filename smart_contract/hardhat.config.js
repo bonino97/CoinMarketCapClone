8fdd453179caf3b1435b2442f8053cf763f689ab
bonino97 <juanbonino97@gmail.com>
2022-05-14 12:40:18 -0300

    Deploy and add token.s

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,14 @@
-require('@nomiclabs/hardhat-waffle')
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
-  solidity: '0.8.4',
+  solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: process.env.SPEEDY_NODE,
-      accounts: [process.env.ACCOUNT],
+      url: "https://speedy-nodes-nyc.moralis.io/15393db2df480e6f82e4bf86/eth/rinkeby",
+      accounts: [
+        "3937c8148b06043dcd2822580179cdd415e587dfc1f4a848955afeea36180ca0",
+      ],
     },
   },
-}
+};
